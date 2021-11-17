f0e9261ecab56427f73951430f910ca9e521f864
Raul de Hevia <raul.dehevia@gmail.com>
2021-11-16 18:11:03 -0800

    Chart now fits in a 16:9 screen
    Co-authored-by: Alex Drover <alexfdrover@gmail.com>
    Co-authored-by: Minh Vu <minhphanh.vu@gmail.com>
    Co-authored-by: Stephanie Cunnane <stephanie.cunnane@gmail.com>

--- src/components/WindowChart.js
+++ src/components/WindowChart.js
@@ -13,65 +13,86 @@ import {
   VictoryScatter,
 } from "victory";
 
-const WindowChart = ({ 
-  responseTime, maxResponseTime, concurrentUsers, maxConcurrentUsers,
-  transactionRate, maxTransactionRate, passRatio, maxPassRatio,
-  zoomDomain, setZoomDomain, active
+const WindowChart = ({
+  responseTime,
+  maxResponseTime,
+  concurrentUsers,
+  maxConcurrentUsers,
+  transactionRate,
+  maxTransactionRate,
+  passRatio,
+  maxPassRatio,
+  zoomDomain,
+  setZoomDomain,
+  active,
 }) => {
-  const { isActiveResponseTime, isActiveConcurrentUsers, isActiveTransactionRate, isActivePassRatio } = active;
+  const {
+    isActiveResponseTime,
+    isActiveConcurrentUsers,
+    isActiveTransactionRate,
+    isActivePassRatio,
+  } = active;
   return (
     <>
       <VictoryChart
         theme={VictoryTheme.material}
         domainPadding={20}
-        padding={{left:100, right:100, top: 10, bottom: 50}}
+        padding={{ left: 100, right: 100, top: 10, bottom: 50 }}
         width={1000}
-        height={150}
+        height={110}
         scale={{ x: "time" }}
         domain={{ y: [0, 1.1] }}
         containerComponent={
           <VictoryBrushContainer
-              brushDimension="x"
-              brushDomain={zoomDomain}
-              onBrushDomainChange={(domain) =>
-                setZoomDomain({ x: domain.x, y: [0, 1.1] })
-              }
-            />
+            brushDimension="x"
+            brushDomain={zoomDomain}
+            onBrushDomainChange={(domain) =>
+              setZoomDomain({ x: domain.x, y: [0, 1.1] })
+            }
+          />
         }
-      > 
+      >
         <VictoryAxis />
 
-        {isActiveResponseTime && <VictoryLine
-          name="Response Time"
-          style={{ data: { stroke: "tomato" } }}
-          data={responseTime}
-          x={"time"}
-          y={(datum) => datum.value / maxResponseTime}
-        />}
+        {isActiveResponseTime && (
+          <VictoryLine
+            name="Response Time"
+            style={{ data: { stroke: "tomato" } }}
+            data={responseTime}
+            x={"time"}
+            y={(datum) => datum.value / maxResponseTime}
+          />
+        )}
 
-        {isActiveConcurrentUsers&&<VictoryLine
-          style={{ data: { stroke: "blue" } }}
-          data={concurrentUsers}
-          x={"time"}
-          y={(datum) => datum.value / maxConcurrentUsers}
-        />}
-        
-        {isActiveTransactionRate&&<VictoryLine
-          style={{ data: { stroke: "purple" } }}
-          data={transactionRate}
-          x={"time"}
-          y={(datum) => datum.value / maxTransactionRate}
-        />}
-        
-        {isActivePassRatio&&<VictoryLine
-          style={{ data: { stroke: "green" } }}
-          data={passRatio}
-          x={"time"}
-          y={(datum) => datum.value / maxPassRatio}
-        />}
+        {isActiveConcurrentUsers && (
+          <VictoryLine
+            style={{ data: { stroke: "blue" } }}
+            data={concurrentUsers}
+            x={"time"}
+            y={(datum) => datum.value / maxConcurrentUsers}
+          />
+        )}
+
+        {isActiveTransactionRate && (
+          <VictoryLine
+            style={{ data: { stroke: "purple" } }}
+            data={transactionRate}
+            x={"time"}
+            y={(datum) => datum.value / maxTransactionRate}
+          />
+        )}
+
+        {isActivePassRatio && (
+          <VictoryLine
+            style={{ data: { stroke: "green" } }}
+            data={passRatio}
+            x={"time"}
+            y={(datum) => datum.value / maxPassRatio}
+          />
+        )}
       </VictoryChart>
     </>
-  )
-}
+  );
+};
 
-export default WindowChart
+export default WindowChart;
