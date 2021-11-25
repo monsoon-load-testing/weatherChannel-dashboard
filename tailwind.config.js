824db9c5c414fb77b4e140e5c400cb4950ab2408
Alex Drover <alexfdrover@gmail.com>
2021-11-24 21:04:11 -0800

    Add custom colors as per graphic designer
    Co-authored-by: Raul de Hevia <raul.dehevia@gmail.com>
    Co-authored-by: Stephanie Cunnane <stephanie.cunnane@gmail.com>
    Co-authored-by: Minh Phan Hvu <minhphanh.vu@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      indigo: {
+        DEFAULT: '#1D0D5C'
+      },
+      mediumPurple: {
+        DEFAULT: '#916cbf'
+      },
+      cornFlowerBlue: {
+        DEFAULT: '#649CD9'
+      },
+      monsoonGreen: {
+        DEFAULT: 'rgba(110, 231, 183, var(--tw-bg-opacity))'
+      },
+    },
   },
   variants: {
     extend: {},
